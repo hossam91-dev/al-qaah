3ec6cc2a7f96afe9b307cffddecafacea39a18fd
abdalla shiha <hossamkidr163@gmail.com>
2026-04-20 00:09:26 +0200

    chore: setup supabase and dotenv integration

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:al_qaah/core/router/app_router.dart';
 import 'package:al_qaah/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:al_qaah/l10n/app_localizations.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   configureDependencies();
   runApp(const MyApp());
 }
